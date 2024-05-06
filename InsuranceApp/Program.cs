d4db1489d4ff9e59c77124765afa801e4eab505c
Joel Siju <joelsiju@stjohns.school.nz>
2024-05-07 11:09:37 +1200

    error messages done and made global variable readonly

--- InsuranceApp/Program.cs
+++ InsuranceApp/Program.cs
@@ -2,108 +2,205 @@
 
 //main process or when run --- above main method
 using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace InsuranceApp
 {
-   
     internal class Program
     {
         //global variable
-        static List<string> deviceCategory = new List<string>()
-        
+        readonly static List<string> DEVICECATEGORY = new List<string>()
+
         {
           "Laptop", "Desktop", "Other(such as a smartphone or drone)"
         };
         
-        static int laptopCounter, desktopCounter, otherCounter;
+
+
+        static int laptopCounter = 0, desktopCounter = 0, otherCounter = 0;
         static string mostExpensiveDeviceName;
-        static float totalInsuranceCost, mostExpensiveDevice = 0;
-      
+        static float totalInsuranceCost = 0, mostExpensiveDevice = 0;
+        private static string menu;
+
         //methods and functions
+
+        static float Checkfloat(string question, float min, float max)
+
+        {
+            while (true)
+
+                try
+
+                {
+
+                    Console.WriteLine(question);
+
+                    float userfloat = (float)Convert.ToDecimal(Console.ReadLine());
+
+                    if (userfloat >= min && userfloat <= max)
+
+                    {
+                        return userfloat;
+                    }
+
+                    DisplayErrorMessage($"ERROR: You must enter an integer between {min} and {max}");
+
+                }
+
+                catch
+
+                {
+                    DisplayErrorMessage($"ERROR: You must enter an integer between {min} and {max}");
+                }
+            static void DisplayErrorMessage(string error)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(error);
+                Console.ForegroundColor = ConsoleColor.White;
+
+            }
+        }
+
+        static int Checkint(string question, int min, int max)
+        {
+            while (true)
+
+                try
+
+                {
+
+
+
+                    Console.WriteLine(question);
+
+                    int userint = Convert.ToInt32(Console.ReadLine());
+
+                    if (userint >= min && userint <= max)
+
+                    {
+                        return userint;
+                    }
+
+                    DisplayErrorMessage($"ERROR: You must enter an integer between {min} and {max}");
+                }
+
+                catch
+
+                {
+                    DisplayErrorMessage($"ERROR: You must enter an integer between {min} and {max}");
+                }
+            static void DisplayErrorMessage(string error)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(error);
+                Console.ForegroundColor = ConsoleColor.White;
+
+            }
+        }
+
+
         static void OneDevice()
         {
+            
 
-
-           
             string deviceName;
             int numOfDevice, deviceCategory;
             float deviceCost, depreciation, insuranceCost = 0;
-            
+
+
+           
+
+
+
+
+            static void DisplayErrorMessage(string error)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(error);
+                Console.ForegroundColor = ConsoleColor.White;
+
+            }
+
 
             //User enters Device Name
             Console.WriteLine("Enter Device's name:");
-            deviceName  = Console.ReadLine();
+            deviceName = Console.ReadLine();
+             
+            if (string.IsNullOrEmpty(deviceName))
+
+            {
+                DisplayErrorMessage("ERROR: Device Name must be entered");
+                OneDevice();
+                return;
+            }
             Console.WriteLine();
-            
+
+
+
 
             //User enters category of device
+            int categoryCount = 0;
             Console.WriteLine("Enter Device Category:");
             Console.WriteLine("pick a number 1, 2, or 3");
             Console.WriteLine("1.Laptop");
             Console.WriteLine("2.Desktop");
             Console.WriteLine("3.Other(such as a smartphone or drone):");
-            deviceCategory = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine();
+            deviceCategory = Checkint(menu, 1, 3);
+
+
+
+
+            //user enters cost for device
+            deviceCost = Checkfloat("Enter the Device cost:\n", 0, 1000000);
 
 
             //User enters number of devices used at school
-            Console.WriteLine("Enter number of devices used at school:");
-            numOfDevice = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine();
-
-            //user enters cost for device
-            Console.WriteLine("Enter Device Cost:");
-            deviceCost = (float)Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine();
-
-
-
-
-
-
-
-
+            numOfDevice = Checkint("Enter the number of devices used at school:\n", 0, 1000000);
+         
+            
+          
             //calculate insurance
             if (numOfDevice > 5)
             {
                 insuranceCost += deviceCost;
                 insuranceCost = +(numOfDevice - 5);
-             
+
             }
 
             if (numOfDevice < 6)
             {
-              insuranceCost += (numOfDevice - 5) * deviceCost * 0.9f;
+                insuranceCost += (numOfDevice - 5) * deviceCost * 0.9f;
             }
             else
             {
                 insuranceCost += numOfDevice * deviceCost;
-                
+
             }
             //display summary
             Console.WriteLine($" {deviceName}");
             Console.WriteLine($"total cost for {numOfDevice} x {deviceName} is  =  ${insuranceCost} ");
 
             //calculate depreciation over 6 months
-            Console.WriteLine("Month   Value Loss");
             Console.WriteLine("Depreciation value for device along 6 month period");
+            Console.WriteLine("Month   Value Loss");
+            
             depreciation = deviceCost;
             for (int month = 0; month < 6; month++)
 
             {
-                
+
                 depreciation = depreciation * 0.95f;
                 depreciation = (float)Math.Round(depreciation, 2);
                 Console.WriteLine($"{month + 1}\t ${depreciation}");
 
             }
 
-            Console.WriteLine($"Category:{deviceCategory}");
+            Console.WriteLine($"Category:{DEVICECATEGORY[deviceCategory-1]}");
 
-            
 
-            //display how many of each  category
+
+            //calculate how many of each  category
             if (deviceCategory.Equals(1))
             {
                 laptopCounter += numOfDevice;
@@ -125,62 +222,73 @@ namespace InsuranceApp
             if (insuranceCost > mostExpensiveDevice)
             {
                 mostExpensiveDevice = insuranceCost;
-                mostExpensiveDeviceName = deviceName;  
+                mostExpensiveDeviceName = deviceName;
             }
+        }
 
-           
+      
 
+        static string CheckProceed()
+        {
+
+            while (true)
+            {
+                Console.WriteLine("press <Enter> to add another device or type 'X' to exit");
+                string checkProceed = Console.ReadLine();
+
+                checkProceed = checkProceed.ToUpper();
+                if (checkProceed.Equals("") || checkProceed.Equals("X"))
+                {
+                    return checkProceed;
+                }
+
+                DisplayErrorMessage("Error invalid response");
+            }
+        }
+        static void DisplayErrorMessage(string error)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(error);
+            Console.ForegroundColor = ConsoleColor.White;
 
         }
+
+
+    
+
 
         //Main process
         static void Main(string[] args)
-        {
-            //display app title
-            Console.WriteLine("Insurance App");
-            Console.WriteLine();
-            
-            string proceed = "";
-            while (proceed.Equals(""))
             {
-                //call OneDevice()
-                OneDevice();
-
-                Console.WriteLine("Press <ENTER> To Add Another Device or type X to Exit");
-                proceed = Console.ReadLine();
+                //display app title
+                Console.WriteLine("Insurance App");
                 Console.WriteLine();
+                string proceed = "";
+                while (proceed.Equals(""))
+                {
+                    OneDevice();
+                    proceed = CheckProceed();
+
+                }
+                //display second part of summary
+                Console.WriteLine($"The number of Laptops: {laptopCounter}");
+                Console.WriteLine($"The number of Desktops: {desktopCounter}");
+                Console.WriteLine($"The number of other Device: {otherCounter}");
+
+                //display total value for insurance
+                Console.WriteLine($"The total value for Insurance:${totalInsuranceCost}");
+
+                //display the most expensive device
+                Console.WriteLine($"The most expensive device - {mostExpensiveDeviceName} costing ${mostExpensiveDevice}");
+
+
 
             }
-            //display second part of summary
-            Console.WriteLine($"The number of Laptops: {laptopCounter}");
-            Console.WriteLine($"The number of Desktops: {desktopCounter}");
-            Console.WriteLine($"The number of other Device: {otherCounter}");
-
-            //display total value for insurance
-            Console.WriteLine($"The total value for Insurance:{totalInsuranceCost}");
-
-            //display the most expensive device
-            Console.WriteLine($"The most expensive device - {mostExpensiveDeviceName} @ {mostExpensiveDevice}");
-
-            
-
-
-
-
-
-
-
-
-
-
-            
-
-
-        }
-
-
-
     }
+}
 
     
-}
+       
+
+
+    
